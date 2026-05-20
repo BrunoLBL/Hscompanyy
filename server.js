f263d5e194a9efacc22215a4f56d5c36b817b90e
Lázaro Bruno <bruninlima77gg@gmail.com>
2026-05-20 17:41:23 -0300

    Otimiza puppeteer para evitar crash de memoria no Render

--- server.js
+++ server.js
@@ -35,7 +35,17 @@ let isReady = false;
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu',
+            '--single-process'
+        ]
     }
 });
 
